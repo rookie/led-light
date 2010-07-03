d5a3ac1d3188e81e9d977c6bd3f9293a64e4ed95
rookie <rookie@win7.local>
2010-07-02 23:13:54 -0700

    timer0 testing

--- main.c
+++ main.c
@@ -1,11 +1,7 @@
 
-//#include "io430.h"
-//#include "msp430.h"
+
+#include "msp430.h"
 /*
-
-defined (__MSP430F2013__)
-#include "io430x20x3.h"
-
 defined (__MSP430F2013__)
 #include "MSP430F2013.h"
 */
@@ -19,8 +15,6 @@ Timer_A capture/compare control 0 TACCTL0   Read/write 0162h  Reset with POR
 Timer_A capture/compare 0         TACCR0    Read/write 0172h  Reset with POR
 Timer_A capture/compare control 1 TACCTL1   Read/write 0164h  Reset with POR
 Timer_A capture/compare 1         TACCR1    Read/write 0174h  Reset with POR
-Timer_A capture/compare control 2 TACCTL2†  Read/write 0166h  Reset with POR
-Timer_A capture/compare 2         TACCR2†   Read/write 0176h  Reset with POR
 Timer_A interrupt vector          TAIV      Read only  012Eh  Reset with POR
 */
 
@@ -30,19 +24,20 @@ Timer_A interrupt vector          TAIV      Read only  012Eh  Reset with POR
 
 
 
-#include <msp430x20x3.h>
+//#include <msp430x20x3.h>
 
 void main(void)
 {
   WDTCTL = WDTPW + WDTHOLD;                 // Stop WDT
   P1DIR |= 0x01;                            // P1.0 output
 
-  CCTL0 = CCIE;                             // CCR0 interrupt enabled
-  CCR0 = 50000;
+  TACCTL0 = CCIE;                             // CCR0 interrupt enabled
+  //TACCR0 = 0xFF;
   TACTL = TASSEL_2 + MC_2;                  // SMCLK, contmode
+  //TACTL = TASSEL_2 + ID_0 + MC_1;             // SMCLK, /8 upmode
+  //TACTL = TASSEL_1 + ID_3 + MC_1;             // ACLK, /8 upmode
 
-  _BIS_SR(LPM0_bits + GIE);                 // Enter LPM0 w/ interrupt
-  for(;;);
+  _BIS_SR(LPM0_bits + GIE);                 // Enter LPM0 w/ interrupts
 }
 
 // Timer A0 interrupt service routine
@@ -50,7 +45,6 @@ void main(void)
 __interrupt void Timer_A (void)
 {
   P1OUT ^= 0x01;                            // Toggle P1.0
-  CCR0 += 50000;                            // Add Offset to CCR0
 }
 
 
@@ -75,41 +69,3 @@ __interrupt void Timer_A (void)
 
 
 
-
-#if 0
-void sw_delay(unsigned int i);
-
-int main( void )
-{
-  // Stop watchdog timer to prevent time out reset
-  WDTCTL = WDTPW + WDTHOLD;
-  P1DIR |= 0x01;                        // Set P1.0 to output direction
-
-  SetupP4
-  bis.b #00Eh,&P4SEL ; Select TB1, TB2, TB3 instead of
-  bis.b #00Eh,&P4DIR ; P4.x, and set as outputs
-  P4SEL |= 0x0E;
-  P4DIR |= 0x0E;
-    
-  /*firgure out how to get these set*/
-  TACTL = TASSEL1 | TASSEL0 |
-          ID1 | ID0 | 
-          MC1 | MC0 |
-          TACLR | TAIE | TAIFG;
-  TACCTL0 = CM1 | CM0 |
-            CCIS1 | CCIS0 |
-            SCS | SCCI | CAP |
-            OUTMOD2 | OUTMOD1 | OUTMOD0 |
-            CCIE | CCI | OUT | COV | CCIFG;
-  
-  for (;;)
-  {
-    P1OUT ^= 0x01;                      // Toggle P1.0 using exclusive-OR
-    sw_delay(20000);                          // SW Delay
-  }
-}
-
-void sw_delay(volatile unsigned int i){
-  while(i-- != 0);
-}
-#endif
